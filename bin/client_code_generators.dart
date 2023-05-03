8ee44f61e167fbb1b789bdd5c7b069d00c49fa74
Cirrus CI <support@cirruslabs.org>
2023-05-03 21:15:51 +0000

    feat: :sparkles: add setup

---
+++ bin/client_code_generators.dart
@@ -0,0 +1,5 @@
+import 'package:client_code_generators/client_code_generators.dart' as client_code_generators;
+
+void main(List<String> arguments) {
+  print('Hello world: ${client_code_generators.calculate()}!');
+}
---
+++ test/client_code_generators_test.dart
@@ -0,0 +1,8 @@
+import 'package:client_code_generators/client_code_generators.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
