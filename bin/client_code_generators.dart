f8f83025de9346f77e4218a0b7134e0431f42075
Cirrus CI <support@cirruslabs.org>
2023-05-05 21:33:22 +0000

    chore: :fire:

--- bin/client_code_generators.dart
+++
@@ -1,5 +0,0 @@
-import 'package:client_code_generators/client_code_generators.dart' as client_code_generators;
-
-void main(List<String> arguments) {
-  print('Hello world: ${client_code_generators.calculate()}!');
-}
