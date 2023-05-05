cc29b6d470d83e37508797c58ae0cb1d287e2a98
Cirrus CI <support@cirruslabs.org>
2023-05-05 21:34:25 +0000

    feat: :sparkles:

---
+++ lib/src/language.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:client_code_generators/src/code/dart-http/dartHttpLanguageMap.dart';
+
+var languageMap =  [
+  dartHttpLanguageMap,
+];
+
+var labelList = {
+  "dart": "Dart"
+};
