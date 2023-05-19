0b1e81497b58013c3681841a3b222a888f284e58
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-05-19 07:40:30 +0100

    test: add parse url encoded body disabled false

---
+++ lib/src/code/dart-http/test/parse_url_encoded_body_test.dart
@@ -0,0 +1,19 @@
+import 'package:client_code_generators/src/code/dart-http/data_body.dart';
+import 'package:test/test.dart';
+
+void main() {
+  var dataBody = DataBody();
+
+  test('should use parse url encoded body disabled false', () {
+    var body = {
+      "mode": "urlencoded",
+      "urlencoded": [
+        {"key": "name", "value": "body", "disabled": false}
+      ]
+    };
+    var contentType = 'application/x-www-form-urlencoded';
+    var raw = dataBody.parseBody(body, contentType, false, 2);
+    expect(raw, 'request.bodyFields = {\n  \'name\': \'body\'\n};');
+  });
+
+}
